0020157ccce557d69016044968cad182ccf2a49e
Hamid <mail.hamid.hoseini@gmail.com>
2022-09-13 23:43:06 -0700

    change next-auth to netx-auth@beta

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,24 @@
 import { getProviders, signIn } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 
 function Login({ providers }) {
+  console.log(providers);
+
+  // const [providers, setProviders] = useState(null);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const res = await getProviders();
+  //     setProviders(res);
+  //     console.log(res);
+  //   })();
+  // }, []);
+
   return (
     <div className='flex flex-col items-center bg-black min-h-screen w-full justify-center'>
       <img className='w-52 mb-5' src="https://1000logos.net/wp-content/uploads/2021/04/Spotify-logo.png" />
 
-      {Object.values(providers).map((provider) => (
+      {providers && providers.length && Object.values(providers).map((provider) => (
         <div key={provider.name}>
           <button className='bg-[#18D860] text-white p-5 rounded-full'
             onClick={() => signIn(provider.id, {callbackUrl: '/'})}
@@ -13,6 +26,7 @@ function Login({ providers }) {
         </div>
       ))}
     </div>
+
 
   )
 }
