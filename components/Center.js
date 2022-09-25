04793ddadbba88dc3745176bb811134b3b7a3970
Hamid <mail.hamid.hoseini@gmail.com>
2022-09-24 20:19:59 -0700

    add songs component

--- components/Center.js
+++ components/Center.js
@@ -5,6 +5,7 @@ import useSpotify from '../hooks/useSpotify';
 import { useEffect, useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import Songs from '../components/Songs';
 
 const colors = [
   'from-indigo-500',
@@ -41,7 +42,7 @@ function Center() {
 
   console.log(playlist);
   return (
-    <div className='flex-grow text-white'>
+    <div className='flex-grow h-screen overflow-y-scroll scrollbar-hide text-white'>
       <header className="absolute top-5 right-8">
         <div className="flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2">
           <img className="rounded-full w-10 h-10" src={session?.user?.image} alt="" />
@@ -49,9 +50,19 @@ function Center() {
           <ChevronDownIcon className="h-5 w-5" />
         </div>
       </header>
-      <section className={`flex items-end space-x-7 bg-gradient-to-b to to-black ${color} h-80 text-white padding-8`}>
-        <h1>Hello</h1>
+      <section className={`flex items-end space-x-7 bg-gradient-to-b to to-black ${color} h-80 text-white p-8`}>
+        <img className="h-44 w-44 shadow-2xl" src={playlist?.images[0]?.url} alt="" />
+        <div>
+          <p>PLAYLIST</p>
+          <h1 className="text-2xl md:text-3xl xl:text-5xl font-bold">
+            {playlist?.name}
+          </h1>
+        </div>
+        
       </section>
+      <div>
+        <Songs />
+      </div>
     </div>
   )
 }
